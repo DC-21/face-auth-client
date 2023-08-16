001e434aceb5d2bfb3693df7561e47e43214b23b
DC-21 <cholahkuboko@gmail.com>
2023-08-16 10:09:47 +0200

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
 
 const Login = () => {
   return (
-    <div>Login Please</div>
+    <div className="w-full h-screen flex justify-center items-center bg-white">
+        <div className="flex justify-center items-center w-[400px] h-[500px] border-2 border-blue-900 bg-white rounded">
+            <h1 className="text-blue-900 text-xl font-semibold">Login Please</h1>
+        </div>
+    </div>
   )
 }
 
