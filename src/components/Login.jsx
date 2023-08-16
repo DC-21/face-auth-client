9a11e4ba517a24278e633214b7766dfb9a68099b
DC-21 <cholahkuboko@gmail.com>
2023-08-16 10:56:26 +0200

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,40 @@
-
 const Login = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center bg-white">
-        <div className="flex flex-col justify-center items-center w-[400px] h-[500px] border-2 border-blue-900 bg-white rounded">
-            <h1 className="text-blue-900 text-xl font-semibold">Login Please</h1>
-            <div className="w-full flex flex-col justify-center items-center">
-                <div className="w-full flex flex-col justify-center items-center mt-2">
-                    <label className="mt-2 text-blue-800">Email</label>
-                    <input type="email" className="h-10 mt-2 border rounded text-blue-800 border-blue-900 justify-center" placeholder="email"/>
-                </div>
-                <div className="w-full flex flex-col justify-center items-center mt-2">
-                    <label className="mt-2 text-blue-800">Password</label>
-                    <input type="password" className="h-10 mt-2 border rounded text-blue-800 border-blue-900 justify-center" placeholder="password"/>
-                </div>
-            </div>
+      <div className="flex flex-col justify-center items-center w-[400px] h-[500px] border-2 border-blue-900 bg-white rounded">
+        <h1 className="text-blue-900 text-xl font-semibold">Login Please</h1>
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="w-full flex flex-col justify-center items-center mt-2">
+            <label className="mt-2 text-blue-800">Email</label>
+            <input
+              style={{ textAlign: "center", color: "blue" }}
+              type="email"
+              className="h-10 mt-2 border rounded text-blue-800 border-blue-900 justify-center"
+              placeholder="email"
+            />
+          </div>
+          <div className="w-full flex flex-col justify-center items-center mt-2">
+            <label className="mt-2 text-blue-800">Password</label>
+            <input
+              style={{ textAlign: "center", color: "blue" }}
+              type="password"
+              className="h-10 mt-2 border rounded text-blue-800 border-blue-900 justify-center"
+              placeholder="password"
+            />
+          </div>
+          <div className="w-full flex flex-col justify-center items-center mt-2">
+            <button className="py-2 px-4 rounded flex justify-center bg-blue-700 text-white">
+              Log in
+            </button>
+            <p>or use</p>
+            <button className="py-2 px-4 rounded flex justify-center bg-orange-500 text-white">
+              FaceID
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
